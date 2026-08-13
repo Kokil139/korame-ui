97a7246195f5bed27fd2de09319215ec620a4ed5
Kokil Rana <54205265+Kokil139@users.noreply.github.com>
2026-08-13 14:55:45 +0530

    Go Live

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    // Replace 'korame-ui' with your GitHub repository name if different
-    base: process.env.NODE_ENV === 'production' ? '/',
+  plugins: [react()],
+  base: '/',
 })
